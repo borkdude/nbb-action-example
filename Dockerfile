9a7242bc19f6bb0462e082af6853f257fa0da4f8
Michiel Borkent <michielborkent@gmail.com>
2021-09-12 21:18:03 +0200

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:16.5.0-alpine
 COPY node_modules node_modules
-COPY action.cljs .
-COPY index.mjs .
+COPY action.cljs /action.cljs
+COPY index.mjs /index.mjs
 ENTRYPOINT [ "node", "index.mjs" ]
--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
 import { loadFile } from 'nbb';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 console.log(resolve('action.cljs'));
 await loadFile('action.cljs');
