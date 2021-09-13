005689c046bc5cfcfd8084749f54db926fcdf472
Michiel Borkent <michielborkent@gmail.com>
2021-09-13 09:54:46 +0200

    yo

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM node:16.5.0-alpine
 COPY package.json /package.json
 RUN  npm install
 COPY action.cljs /action.cljs
-COPY index.mjs /index.mjs
+COPY dist/index.mjs /index.mjs
 ENTRYPOINT [ "node", "/index.mjs" ]
---
+++ dist/action.cljs
@@ -0,0 +1,11 @@
+(fn action [core github]
+  (try
+    (let [name-to-greet (.getInput core "who-to-greet")
+          _ (.log js/console (str "Hello " name-to-greet "!"))
+          time (.toTimeString (js/Date.))
+          _ (.setOutput core "time" time)
+          payload (.. github -context -payload)
+          payload (js/JSON.stringify payload nil 2)
+          _ (.log js/console (str "The event payload: " payload))])
+    (catch :default e
+      (.setFailed core (.-message e)))))
