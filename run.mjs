934fc4b93a5dc3ce5f2ce2fb9a141665b7d62378
Michiel Borkent <michielborkent@gmail.com>
2021-09-16 11:31:21 +0200

    test

---
+++ run.mjs
@@ -0,0 +1,6 @@
+import * as child from 'child_process';
+
+console.log(process.version);
+child.exec('which node', (err,stdout,sterr) => console.log(stdout))
+child.exec('node --version', (err,stdout,sterr) => console.log(stdout))
+child.exec('ls /', (err,stdout,sterr) => console.log(stdout));
