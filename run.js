d20aa696b20215d972e496a916ab10428169b895
Michiel Borkent <michielborkent@gmail.com>
2021-09-16 11:37:43 +0200

    node

--- run.js
+++ run.js
@@ -1,6 +1,2 @@
 child = require('child_process');
-
-console.log(process.version);
-child.exec('which node', (err,stdout,sterr) => console.log(stdout));
-child.exec('node --version', (err,stdout,sterr) => console.log(stdout));
-child.exec('ls /', (err,stdout,sterr) => console.log(stdout));
+child.exec('node dist/index.mjs', (err,stdout,sterr) => console.log(stdout));
