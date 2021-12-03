4838bb82ee140b3c74b49a0021601ed258634bd0
Michiel Borkent <michielborkent@gmail.com>
2021-12-03 17:03:37 +0100

    wip

--- run.cjs
+++ run.cjs
@@ -1,6 +1,6 @@
 child = require('child_process');
 path = require('path');
-
+console.log(process.version);
 // fork to system-installed node which is v14.17.6 at this time of writing
 // this workaround can be removed once the Github actions runner supports a newer version of Node.js
 child.fork(path.resolve(__dirname, 'dist/index.mjs'), [], {execPath: "node"});
