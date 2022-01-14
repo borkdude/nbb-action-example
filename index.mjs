eb37f945eb8417b76320b93548e8c3c18e045314
Michiel Borkent <michielborkent@gmail.com>
2022-01-14 12:03:51 +0100

    Ah well, let's just go with node_modules

--- index.mjs
+++ index.mjs
@@ -1,9 +1,3 @@
 import { loadFile } from 'nbb';
-import { resolve, dirname } from 'path';
-import * as actionsCore from '@actions/core';
-import * as actionsGithub from '@actions/github';
-
-const __dirname = dirname(".");
-const theFile = resolve(__dirname, 'action.cljs');
-const { action } = await loadFile(theFile);
-action( { actionsCore, actionsGithub} );
+const { action } = await loadFile('action.cljs');
+action();
---
+++ node_modules/@octokit/types/dist-node/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const VERSION = "6.27.0";
+
+exports.VERSION = VERSION;
+//# sourceMappingURL=index.js.map
