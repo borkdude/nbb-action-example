c1f05cf15535636b3ea47fd310b89083721436eb
Michiel Borkent <michielborkent@gmail.com>
2022-06-27 23:45:13 +0200

    wip

--- setup.mjs
+++ setup.mjs
@@ -2,9 +2,11 @@ import { execSync } from 'child_process';
 
 import { resolve, dirname } from 'path';
 
-const __dirname = dirname(".");
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const theFile = resolve(__dirname, 'node_modules.zip');
 
 execSync(`ls -la ${__dirname}`, {stdio: 'inherit'});
 
-execSync(`unzip -o node_modules.zip ${__dirname}`, {stdio: 'inherit'});
+execSync(`unzip -o node_modules.zip -d ${__dirname}`, {stdio: 'inherit'});
