6f341048f18f7f6bfa73b97865e868cb8665073d
Michiel Borkent <michielborkent@gmail.com>
2022-06-27 23:48:31 +0200

    wip

--- index.mjs
+++ index.mjs
@@ -3,7 +3,9 @@ import { resolve, dirname } from 'path';
 import * as actionsCore from '@actions/core';
 import * as actionsGithub from '@actions/github';
 
-const __dirname = dirname(".");
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const theFile = resolve(__dirname, 'action.cljs');
 const { action } = await loadFile(theFile);
 action( { actionsCore, actionsGithub} );
