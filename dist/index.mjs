b00f6a563744cf442c4f22fc95a4cee513100d5c
Michiel Borkent <michielborkent@gmail.com>
2022-06-27 23:52:03 +0200

    simplify action

--- dist/index.mjs
+++
@@ -1,9 +0,0 @@
-import { loadFile } from 'nbb';
-import { resolve, dirname } from 'path';
-import * as actionsCore from '@actions/core';
-import * as actionsGithub from '@actions/github';
-
-const __dirname = dirname(".");
-const theFile = resolve(__dirname, 'action.cljs');
-const { action } = await loadFile(theFile);
-action( { actionsCore, actionsGithub} );
