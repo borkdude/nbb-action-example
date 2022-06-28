235cfdbb16fa49f838b5f58375368c8650c6bf53
Michiel Borkent <michielborkent@gmail.com>
2022-06-28 13:15:13 +0200

    Revert "Download node modules from Github releases (#6)"
    This reverts commit 0fa0a010864db4814b0b3f6a4c6a58f151ec5d98.

--- setup.mjs
+++ setup.mjs
@@ -1,20 +1,10 @@
 import { execSync } from 'child_process';
+
 import { resolve, dirname } from 'path';
-import { readFileSync } from 'fs';
 
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const node_modules = resolve(__dirname, 'node_modules.zip');
-const pkg = JSON.parse(readFileSync(resolve(__dirname, 'package.json')).toString());
-const version = pkg.version;
+const node_modules = resolve(__dirname, 'dist/node_modules.zip');
 
-console.log(version);
-
-execSync(`curl -sLO https://github.com/borkdude/nbb-action-example/releases/download/v${version}/node_modules.zip`, {
-  cwd: __dirname,
-  stdio: 'inherit'});
-
-execSync(`unzip -o -q ${node_modules}`, {
-  cwd: __dirname,
-  stdio: 'inherit'});
+execSync(`unzip -o ${node_modules} -d ${__dirname}`, {stdio: 'inherit'});
