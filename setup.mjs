fcc5842cc0f59a20b35d033dad87bf99e2137a15
Michiel Borkent <michielborkent@gmail.com>
2022-06-28 10:22:27 +0200

    wip

--- setup.mjs
+++ setup.mjs
@@ -8,5 +8,5 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // const node_modules = resolve(__dirname, 'dist/node_modules.zip');
 
 execSync(`npm install`, {
-  dir: __dirname,
+  //dir: __dirname,
   stdio: 'inherit'});
