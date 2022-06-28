b5c0d118b8cdc22235b3a7e7e1c5574cde92786c
Michiel Borkent <michielborkent@gmail.com>
2022-06-28 10:42:46 +0200

    download on setup

--- setup.mjs
+++ setup.mjs
@@ -5,6 +5,8 @@ import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const node_modules = resolve(__dirname, 'dist/node_modules.zip');
+const node_modules = resolve(__dirname, 'node_modules.zip');
+
+execSync(`curl -sLO https://github.com/borkdude/nbb-action-example/releases/download/v0.1.0/node_modules.zip`, {stdio: 'inherit'});
 
 execSync(`unzip -o ${node_modules} -d ${__dirname}`, {stdio: 'inherit'});
