f5b1dfa1fa816ab00d4faab51dac2c25541b72bf
Michiel Borkent <michielborkent@gmail.com>
2022-06-28 11:49:56 +0200

    yo

--- setup.mjs
+++ setup.mjs
@@ -1,20 +1,20 @@
 import { execSync } from 'child_process';
-
 import { resolve, dirname } from 'path';
+import { readFileSync } from 'fs';
 
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const node_modules = resolve(__dirname, 'node_modules.zip');
-
-import pkg from './package.json' assert {type: "json"};;
-
+const pkg = JSON.parse(readFileSync(resolve(__dirname, 'package.json')).toString());
 const version = pkg.version;
+
+console.log(version);
 
 execSync(`curl -sLO https://github.com/borkdude/nbb-action-example/releases/download/v${version}/node_modules.zip`, {
   cwd: __dirname,
   stdio: 'inherit'});
 
-execSync(`unzip -q ${node_modules}`, {
+execSync(`unzip -o -q ${node_modules}`, {
   cwd: __dirname,
   stdio: 'inherit'});
