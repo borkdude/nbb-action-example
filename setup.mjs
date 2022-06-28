c8a622a7248af9ebcc33465b0f8409ad7693eced
Michiel Borkent <michielborkent@gmail.com>
2022-06-28 10:26:07 +0200

    wip

--- setup.mjs
+++ setup.mjs
@@ -1,15 +1,8 @@
 import { execSync } from 'child_process';
-
 import { resolve, dirname } from 'path';
-
 import { fileURLToPath } from 'url';
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// const node_modules = resolve(__dirname, 'dist/node_modules.zip');
-
-execSync(`ls -la`, {
-  cwd: __dirname,
-  stdio: 'inherit'});
 
 execSync(`npm install`, {
   cwd: __dirname,
