b806932ad53bb755da3d2f744723a6d808e6cb19
Michiel Borkent <michielborkent@gmail.com>
2022-06-28 10:21:08 +0200

    wip

--- setup.mjs
+++ setup.mjs
@@ -5,6 +5,8 @@ import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const node_modules = resolve(__dirname, 'dist/node_modules.zip');
+// const node_modules = resolve(__dirname, 'dist/node_modules.zip');
 
-execSync(`unzip -o ${node_modules} -d ${__dirname}`, {stdio: 'inherit'});
+execSync(`npm install`, {
+  dir: __dirname,
+  stdio: 'inherit'});
